8ff133d7d4d82f0f53c49598908739ad96554f0b
-Lunat1c- <arturgrabowski100@onet.pl>
2023-01-31 22:46:32 +0100

    Small styling fix

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,20 +4,22 @@
     import { page } from "$app/stores";
 </script>
 
-<nav class="px-4 py-3 bg-purple-900 text-white">
-    <a class="mx-2 hover:underline" href="/">Home</a>
-    <a class="mx-2 hover:underline" href="/todos">Todos</a>
-    <a class="mx-2 hover:underline" href="/streaks">Streaks</a>
-    <a class="mx-2 hover:underline" href="/fitness">Fitness</a>
-    <a class="mx-2 hover:underline" href="/finances">Finances</a>
-    <a class="mx-2 hover:underline" href="/about">About</a>
-
-    <div class="bg-black">
+<div class="flex">
+    <nav class="px-4 py-3 bg-purple-900 text-white w-full">
+        <a class="mx-2 hover:underline" href="/">Home</a>
+        <a class="mx-2 hover:underline" href="/todos">Todos</a>
+        <a class="mx-2 hover:underline" href="/streaks">Streaks</a>
+        <a class="mx-2 hover:underline" href="/fitness">Fitness</a>
+        <a class="mx-2 hover:underline" href="/finances">Finances</a>
+        <a class="mx-2 hover:underline" href="/about">About</a>
+    </nav>
+    
+    <div class="bg-black w-fit text-white">
         {#if !$page.data.user}
             <a href="/login">Login</a>
             <a href="/register">Register</a>
         {/if}
-
+    
         {#if $page.data.user}
             <form method="POST" action="/logout" use:enhance={() => {
                 return async ({result}) => {
@@ -25,8 +27,8 @@
                     applyAction(result);
                 }
             }}>
-                <button type="submit">Log out</button>
+                <button class="" type="submit">Log out</button>
             </form>
         {/if}
     </div>
-</nav>
+</div>
