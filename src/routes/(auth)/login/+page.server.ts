3baae5e50b218c0e3f7cee4e317da6d3b0b5d7ce
-Lunat1c- <arturgrabowski100@onet.pl>
2023-02-02 20:50:19 +0100

    fix

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -8,37 +8,37 @@ export const load: PageServerLoad = async ({locals}) => {
     }
 }
 
-const login: Action = async ({ cookies, request }) => {
-    const data = await request.formData();
-    const username: string = data.get('username') as  string;
-    const password: string = data.get('password') as  string;
+export const actions: Actions = { 
+    login: async ({ cookies, request }) => {
+        const data = await request.formData();
+        const username: string = data.get('username') as  string;
+        const password: string = data.get('password') as  string;
+        
+        const user = await db.collection('users').findOne({ username: username });
     
-    const user = await db.collection('users').findOne({ username: username });
-
-    if (!user){
-        return {
-            status: 400,
-            body: {
-                credentials: true,
-            },
-        };
+        if (!user){
+            return {
+                status: 400,
+                body: {
+                    credentials: true,
+                },
+            };
+        }
+    
+        const authToken: string = crypto.randomUUID();
+        const authenticatedUser = await db.collection('users').updateOne(
+            { username: user.username },
+            { $set: {userAuthToken: authToken} }
+        );
+    
+        cookies.set('session', authToken, {
+            path: '/',
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 60 * 60 * 24 * 30
+        });
+    
+        throw redirect(302, '/');
     }
-
-    const authToken: string = crypto.randomUUID();
-    const authenticatedUser = await db.collection('users').updateOne(
-        { username: user.username },
-        { $set: {userAuthToken: authToken} }
-    );
-
-    cookies.set('session', authToken, {
-        path: '/',
-        httpOnly: true,
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60 * 24 * 30
-    });
-
-    throw redirect(302, '/');
 }
-
-export const actions: Actions = { login }
