674cd1b26051710f6a9c1d75b4d7c97aa30c6fcd
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2023-02-11 18:03:30 +0100

    Styling

--- src/lib/components/modelComponents/SuggestionComponent.svelte
+++ src/lib/components/modelComponents/SuggestionComponent.svelte
@@ -2,11 +2,12 @@
 	import { enhance } from "$app/forms";
 	import type { SuggestionModel } from "$lib/models/suggestionModel";
 	import { SuggestionStatus } from "$lib/utils/enums";
+	import { formatDate } from "$lib/utils/helpers";
 
     export let suggestion: SuggestionModel;
 
-    function getClass(): string {
-        let result: string = 'flex flex-row justify-between my-2 px-4 py-2 text-center ';
+    function getBgColorClass(): string {
+        let result: string = '';
 
         switch (suggestion.status) {
             case SuggestionStatus.NOT_TOUCHED:
@@ -28,23 +29,38 @@
 
 
 <body>
-    <div class="{getClass()}">
-        {suggestion.usersWhoLikedIds.length} | 
-        {new Date(suggestion.date).toLocaleDateString()} | 
-        {suggestion.description}
+    <div class="flex flex-col my-1 {getBgColorClass()}">
+        <div class="text-gray-700 text-xs m-1">
+            {new Date(suggestion.date).toLocaleDateString()}
+        </div>
+
+        <div class="flex flex-row justify-between my-2 px-4 py-2 text-center">
+            <div class="mr-4">
+                {suggestion.description}
+            </div>
             
-        {#if suggestion.isLikePossible}
-            <form method="POST" action="?/likeSuggestion" use:enhance>
-                <input type="hidden" name="_id" value="{suggestion._id}">
-                <button type="submit" class="btn">+</button>
-            </form>
-        {/if}
+            <div class="flex flex-row border-2">
+                <div class="mr-3">
+                    {suggestion.usersWhoLikedIds.length}
+                </div>
+                {#if suggestion.isLikePossible}
+                    <form method="POST" action="?/likeSuggestion" use:enhance>
+                        <input type="hidden" name="_id" value="{suggestion._id}">
+                        <button type="submit" class="btn">+</button>
+                    </form>
+                {/if}
+            </div>
         
-        {#if suggestion.isOwner}
-            <form method="POST" action="?/deleteSuggestion" use:enhance>
-                <input type="hidden" name="_id" value="{suggestion._id}">
-                <button type="submit" class="btn">Delete</button>
-            </form>
-        {/if}
+            {#if suggestion.isOwner}
+                <form method="POST" action="?/deleteSuggestion" use:enhance>
+                    <input type="hidden" name="_id" value="{suggestion._id}">
+                    <button type="submit" class="btn">Delete</button>
+                </form>
+            {/if}
+        </div>
+
+        <div>
+
+        </div>
     </div>
 </body>
