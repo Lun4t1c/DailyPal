11dd235dc47ca2a05d4c2d98c0adb624b28ad95f
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2023-03-01 16:51:24 +0100

    Added calendar component

---
+++ src/lib/components/modelComponents/CalendarComponent.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+
+<body>
+    <div>Calendar yo</div>
+</body>
---
+++ src/routes/(functionality)/calendar/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import CalendarComponent from "$lib/components/modelComponents/CalendarComponent.svelte";
+
+
+</script>
+
+
+<body>
+    <CalendarComponent></CalendarComponent>
+</body>
