4fc0e0b71014c523f17b7af62c900549660a5a63
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2023-03-01 16:58:00 +0100

    Added props in CalendarComponent

--- src/lib/components/modelComponents/CalendarComponent.svelte
+++ src/lib/components/modelComponents/CalendarComponent.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+	import type { StreakModel } from "$lib/models/streakModel";
+	import type { TodoModel } from "$lib/models/todoModel";
+	import type { TransactionModel } from "$lib/models/transactionModel";
 
+    export let transactions: TransactionModel[] | null = null;
+    export let streaks: StreakModel[] | null = null;
+    export let todos: TodoModel[] | null = null;
 </script>
 
 
