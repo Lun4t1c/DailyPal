4d4f0f59f98e7c41c6f7ec636468728d2f0039e1
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2023-03-01 17:30:12 +0100

    Separated transactions into monthly and one-time

--- src/routes/(functionality)/finances/transactions-list/+page.server.ts
+++ src/routes/(functionality)/finances/transactions-list/+page.server.ts
@@ -1,12 +1,7 @@
-import type { Actions, PageServerLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 import { ObjectId } from "mongodb";
-import { financeSourcesCollection } from "$db/financeSourcesCollection";
-import type { TransactionModel } from "$lib/models/transactionModel";
 import { transactionsCollection } from "$db/transactionsCollection";
-import { stringToBoolean } from "$lib/utils/helpers";
 import { redirect } from "@sveltejs/kit";
-import type { PlannedExpenseModel } from "$lib/models/plannedExpenseModel";
-import { plannedExpensesCollection } from "$db/plannedExpensesCollection";
 
 export const load: PageServerLoad = async function name({locals}) {
     if (!locals.user) throw redirect(302, '/login');
--- src/routes/(functionality)/finances/transactions-list/+page.svelte
+++ src/routes/(functionality)/finances/transactions-list/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { PageData } from "../$types";
+	import TransactionsListEntryComponent from "$lib/components/modelComponents/TransactionsListEntryComponent.svelte";
+    import type { PageData } from "../$types";
 
     export let data: PageData;
     $: ({ transactions } = data)
@@ -8,4 +9,23 @@
 
 <body>
     <div>trans list yo</div>
+
+    <div class="flex flex-row justify-start">
+        <div class="flex flex-col">
+            One-time transactions
+            {#each transactions.filter((t) => { return !t.isMonthly }) as transaction}
+                <TransactionsListEntryComponent transaction={transaction}></TransactionsListEntryComponent>
+            {/each}
+        </div>
+
+        <div class="mx-5"></div>
+    
+        <div class="flex flex-col">
+            Monthly transactions
+            {#each transactions.filter((t) => { return t.isMonthly }) as transaction}
+                <TransactionsListEntryComponent transaction={transaction}></TransactionsListEntryComponent>
+            {/each}
+        </div>
+    </div>
+
 </body>
