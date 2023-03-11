04e11c03afbcb2593cdd7a9ef0fa8d314584d80b
-Lunat1c- <arturgrabowski100@onet.pl>
2023-03-11 15:49:52 +0100

    DRAFT: Updating transaction

--- src/routes/(functionality)/finances/transactions-list/+page.server.ts
+++ src/routes/(functionality)/finances/transactions-list/+page.server.ts
@@ -1,16 +1,69 @@
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { ObjectId } from "mongodb";
+import { financeSourcesCollection } from "$db/financeSourcesCollection";
+import type { TransactionModel } from "$lib/models/transactionModel";
 import { transactionsCollection } from "$db/transactionsCollection";
+import { stringToBoolean } from "$lib/utils/helpers";
 import { redirect } from "@sveltejs/kit";
+import type { PlannedExpenseModel } from "$lib/models/plannedExpenseModel";
 
 export const load: PageServerLoad = async function name({locals}) {
     if (!locals.user) throw redirect(302, '/login');
+
+    const financeSources = await financeSourcesCollection
+        .find({_idUser: new ObjectId(locals.user._id)})
+        .toArray();
 
     const transactions = await transactionsCollection
         .find({_idUser: new ObjectId(locals.user._id)})
         .toArray();
 
     return {
+        financeSources: JSON.parse(JSON.stringify(financeSources)),
         transactions: JSON.parse(JSON.stringify(transactions))
     }
+}
+
+export const actions: Actions = {
+    updateTransaction: async ({request, locals}) => {
+        try {
+            const data = await request.formData();
+            const descriptionTemp: string = data.get('description') as string;
+
+            const transaction: TransactionModel = {
+                _id: new ObjectId(data.get('_id') as string),
+                _idUser: new ObjectId(locals.user._id),
+                _idFinanceSource: new ObjectId(data.get('_idFinanceSource') as string),
+                amountInPennies: Math.floor(parseFloat(data.get('valueInPennies') as string) * 100),
+                description: descriptionTemp !== '' ? descriptionTemp : null,
+                date: 0,
+                isMonthly: false
+            };
+
+            transactionsCollection.updateOne(
+                { _id: new ObjectId(transaction._id) },
+                { $set: { 
+                    _idFinanceSource: transaction._idFinanceSource,
+                    amountInPennies: transaction.amountInPennies,
+                    description: transaction.description,
+                 } }
+            );
+
+            return {
+                status: 200,
+                body: {
+                    status: 'Success'
+                }
+            }
+        }
+        catch (error) {
+            console.log(error);
+            return {
+                status: 500,
+                body: {
+                    status: 'Error'
+                }
+            }
+        }
+    },
 }
--- src/routes/(functionality)/finances/transactions-list/+page.svelte
+++ src/routes/(functionality)/finances/transactions-list/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
+	import Modal, { getModal } from "$lib/components/Modal.svelte";
 	import TransactionsListEntryComponent from "$lib/components/modelComponents/TransactionsListEntryComponent.svelte";
 	import type { TransactionModel } from "$lib/models/transactionModel";
-    import type { PageData } from "../$types";
+	import { ObjectId } from "mongodb";
+    import type { PageData } from "./$types";
 
     export let data: PageData;
-    $: ({ transactions } = data)
+    $: ({ financeSources, transactions } = data)
+
+    let transactionBuffer: TransactionModel = {
+		_idUser: new ObjectId('63ded1bed6656453041c2c83'),
+		_idFinanceSource: new ObjectId('63ded1bed6656453041c2c83'),
+		amountInPennies: 0,
+		description: 'null',
+		date: 0,
+		isMonthly: false
+	};
+    let _idFinanceSource: ObjectId = transactionBuffer._idFinanceSource;
 
     function getSortedOneTimeTransactions(): TransactionModel[] {
         return transactions
@@ -12,6 +25,11 @@
             .sort((a: any, b: any) => {
                 return new Date(b.date) - new Date(a.date);
             });
+    }
+
+    function editTransaction(transaction: TransactionModel): void {
+        transactionBuffer = transaction;
+        getModal('UpdateTransactionModal').open();
     }
 </script>
 
@@ -24,6 +42,9 @@
             One-time transactions
             {#each getSortedOneTimeTransactions() as transaction}
                 <TransactionsListEntryComponent transaction={transaction}></TransactionsListEntryComponent>
+                <button on:click={() => editTransaction(transaction)} class="mb-4">
+                    Edit
+                </button>
             {/each}
         </div>
 
@@ -37,4 +58,40 @@
         </div>
     </div>
 
+    <Modal id="UpdateTransactionModal">
+        <form method="POST" action="?/updatePlannedExpense" use:enhance>
+            <div class="flex flex-col mt-3">
+                <input type="hidden" name="_id" value="{transactionBuffer._id}">
+                <input type="hidden" name="isMonthly" value="{transactionBuffer.isMonthly}">
+                
+                <div class="flex flex-col mt-3">
+                    <label for="date">Date: </label>
+                    <input name="date" id="date" type="date" 
+                        value="{transactionBuffer.date}">
+        
+                    <label for="amount">Amount: </label>
+                    <input name="amount" id="amount" type="number" step="0.01"
+                        value="{transactionBuffer.amountInPennies * 100}">
+    
+                    <label for="description">Description (optional): </label>
+                    <input name="description" id="description" type="text"
+                        value="{transactionBuffer.description}">
+    
+                    <label for="isMonthly">Is monthly: </label>
+                    <input name="isMonthly" id="isMonthly" type="checkbox" 
+                        bind:checked={transactionBuffer.isMonthly}>
+    
+                    {#each financeSources as financeSource}
+                        <label>
+                            <input name="_idFinanceSource" type="radio" value="{financeSource._id}" bind:group="{_idFinanceSource}">
+                            {financeSource.name}
+                        </label>
+                        
+                    {/each}
+        
+                    <button type="submit">
+                        Confirm
+                    </button>
+        </form>
+    </Modal>
 </body>
