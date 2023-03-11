1cc4abb8bb1f9bf2dd1632e1f53023b15dd92c49
-Lunat1c- <arturgrabowski100@onet.pl>
2023-03-11 14:34:59 +0100

    Sorting transactions in list by date reverse

--- src/routes/(functionality)/finances/transactions-list/+page.svelte
+++ src/routes/(functionality)/finances/transactions-list/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import TransactionsListEntryComponent from "$lib/components/modelComponents/TransactionsListEntryComponent.svelte";
+	import type { TransactionModel } from "$lib/models/transactionModel";
     import type { PageData } from "../$types";
 
     export let data: PageData;
     $: ({ transactions } = data)
+
+    function getSortedOneTimeTransactions(): TransactionModel[] {
+        return transactions
+            .filter((t: TransactionModel) => { return !t.isMonthly })
+            .sort((a: any, b: any) => {
+                return new Date(b.date) - new Date(a.date);
+            });
+    }
 </script>
 
 
@@ -13,7 +22,7 @@
     <div class="flex flex-row justify-start">
         <div class="flex flex-col">
             One-time transactions
-            {#each transactions.filter((t) => { return !t.isMonthly }) as transaction}
+            {#each getSortedOneTimeTransactions() as transaction}
                 <TransactionsListEntryComponent transaction={transaction}></TransactionsListEntryComponent>
             {/each}
         </div>
